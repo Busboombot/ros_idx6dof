8dfa292c3d191472f8fd9b4c69bfa520bcb555ca
Eric Busboom <eric@clarinova.com>
2017-06-10 09:15:54 -0700

    .

--- src/idxpendant/src/pendant_mover.py
+++ src/idxpendant/src/pendant_mover.py
@@ -4,11 +4,9 @@ import rospy
 from std_msgs.msg import String as StringMessage
 from pendant import switch_pos, axes_positions
 
-
 def recieve_switch(data):
     
     print switch_pos('speed', data), switch_pos('step', data), axes_positions(data)
-
 
 rospy.init_node('pendant_mover', anonymous=False)
 rospy.Subscriber('pendant_str', StringMessage, recieve_switch)
